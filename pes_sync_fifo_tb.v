e6f16547a2b7e5ccd1864536a8868c29539841bb
Aishwarya <97291384+aishwarya-2511@users.noreply.github.com>
2023-09-01 14:42:33 +0530

    Add files via upload

---
+++ pes_sync_fifo_tb.v
@@ -0,0 +1,81 @@
+
+`timescale 1 ns/ 1 ps
+module iiitb_sfifo_tb();
+	reg CLK;
+	reg RSTn;
+	reg write;
+	reg read;
+	reg [7:0] iData;
+	
+	wire [7:0] oData;
+	wire full;
+	wire empty;
+
+initial
+begin
+	read=0;
+	write=0;
+end
+initial
+begin
+	CLK <= 1'b0;
+	forever #100 CLK <= ~CLK;
+end
+
+
+initial 
+begin
+	RSTn = 0;
+	iData = 0;
+	#10 RSTn = 1;
+end
+	
+always @ (posedge CLK or negedge RSTn)
+begin
+	iData <= iData + 1'b1;
+				  
+end
+
+always @ (posedge CLK or negedge RSTn)
+begin
+	if (!RSTn)
+		write = 0;
+	else if (!full)
+	begin
+		write = 1;
+	end
+	else 
+		write = 0;
+end
+
+always @ (posedge CLK or negedge RSTn)
+begin
+	if (!RSTn)
+		read = 0;
+	else if (!empty)
+		read = 1;
+	else 
+		read = 0;
+end
+
+
+iiitb_sfifo fifo (.CLK(CLK),
+				  .RSTn(RSTn),
+				  .write(write),
+				  .read(read),
+				  .iData(iData),
+				  .full(full),
+				  .empty(empty),
+				  .oData(oData));
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+  end
+  initial
+  #20000 $finish;
+             
+endmodule
+
+
+
